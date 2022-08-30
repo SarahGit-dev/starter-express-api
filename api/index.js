eb48bd53320a61126111841026f15660d96e5bda
SarahGit-dev <82538193+SarahGit-dev@users.noreply.github.com>
2022-08-30 13:20:39 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
 app.all('/api', (req, res) => {
     console.log("Just got a request!")
+    await sleep(60)
     res.send('Yo!')
 })
 app.listen(process.env.PORT || 3000)
