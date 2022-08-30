cd8d49e11e0eca26dcd4ba31898d807b823747c3
SarahGit-dev <82538193+SarahGit-dev@users.noreply.github.com>
2022-08-30 21:40:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const http = require('https');
 const app = express()
-
+const  = process.env.PORT || 3001
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
 
-app.all('/api', (req, res) => {
+app.get('/api', (req, res) => {
     console.log("Just got somthing")
     
     http.get("https://webhook.site/c59ac0e3-533c-472e-ba50-9f8373e86e5a")   
     
     res.send('Yoo!')
 })
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
